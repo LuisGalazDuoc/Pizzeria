4a9a2ddb787141c52f56f8c648ebb5ab975e3aaa
LuisGalazDuoc <lu.galazm@duocuc.cl>
2026-05-10 23:38:15 -0400

    ProyectoV1

---
+++ repartidor-service/src/main/java/com/pizzeria/repartidor_service/RepartidorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pizzeria.repartidor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepartidorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepartidorServiceApplication.class, args);
+	}
+
+}

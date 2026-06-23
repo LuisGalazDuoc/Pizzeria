41a37805bc7a585a55a7c738edef4a5de14b6ff0
Yisuerflower3511 <ad.floresd@duocuc.cl>
2026-06-23 19:01:47 -0400

    Docker implementado

---
+++ clean-all.bat
@@ -0,0 +1,29 @@
+cd api-gateway
+call .\mvnw clean
+
+cd ../auth-service
+call .\mvnw clean
+
+cd ../categoria-service
+call .\mvnw clean
+
+cd ../direccion-service
+call  .\mvnw clean
+
+cd ../envio-service
+call .\mvnw clean
+
+cd ../pago-service
+call .\mvnw clean
+
+cd ../pedido-detalle-service
+call  .\mvnw clean
+
+cd ../pedido-service
+call .\mvnw clean
+
+cd ../producto-service
+call .\mvnw clean
+
+cd ../usuario-service
+call .\mvnw clean
